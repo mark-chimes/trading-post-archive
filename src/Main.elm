01340b877e9bcaaddbcd6c8ca82123e5a0bf267c
Mark Chimes <chimes.mark@gmail.com>
2018-01-17 18:44:34 +0200

    Initial Application
    Structure is loosely based on Clicker.elm available here: https://gist.github.com/hmans/6e8163cc35cab237fa65577b610dd78c

---
+++ src/Main.elm
@@ -0,0 +1,111 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Time exposing (Time, second)
+
+
+-- MODEL
+
+
+type alias Wood =
+    Int
+
+
+type alias Gold =
+    Int
+
+
+type alias GameState =
+    { wood : Wood
+    , gold : Gold
+    }
+
+
+init : ( GameState, Cmd Msg )
+init =
+    let
+        state =
+            { wood = 5
+            , gold = 20
+            }
+    in
+        ( state, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Tick Time
+    | ChopWood
+    | SellWood
+
+
+update : Msg -> GameState -> ( GameState, Cmd Msg )
+update msg state =
+    case msg of
+        Tick _ ->
+            if state.gold > 0 then
+                let
+                    gold =
+                        state.gold - 1
+                in
+                    ( { state | gold = gold }, Cmd.none )
+            else
+                ( state, Cmd.none )
+
+        ChopWood ->
+            ( { state | wood = state.wood + 1 }, Cmd.none )
+
+        SellWood ->
+            if state.wood > 0 then
+                ( { state | gold = state.gold + 5, wood = state.wood - 1 }, Cmd.none )
+            else
+                ( state, Cmd.none )
+
+
+
+-- VIEW
+
+
+viewHeader : String -> Html Msg
+viewHeader title =
+    header []
+        [ h1 [] [ text title ] ]
+
+
+viewGame : GameState -> Html Msg
+viewGame state =
+    div [ id "game" ]
+        [ div []
+            [ text ("Gold: " ++ (toString state.gold)) ]
+        , div []
+            [ text ("Wood: " ++ (toString state.wood))
+            , button [ onClick ChopWood ] [ text "Chop Wood" ]
+            ]
+        , div []
+            [ text ("Sell Wood")
+            , button [ onClick SellWood, disabled (state.wood <= 0) ] [ text ("Sell 1 wood for 5 gold") ]
+            ]
+        ]
+
+
+view : GameState -> Html Msg
+view state =
+    div [ class "content" ]
+        [ viewHeader "Trading Post!"
+        , viewGame state
+        ]
+
+
+subscriptions : GameState -> Sub Msg
+subscriptions state =
+    Time.every second Tick
+
+
+main : Program Never GameState Msg
+main =
+    Html.program { init = init, view = view, update = update, subscriptions = subscriptions }
