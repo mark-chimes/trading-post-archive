e1ca5453caacd9bed4414f59a7572262b889f534
Mark Chimes <chimes.mark@gmail.com>
2018-02-23 09:58:20 +0200

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './main.css';
-import { Main } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
-
-var node = document.getElementById('root');
-var app = Main.embed(node);
-registerServiceWorker();
