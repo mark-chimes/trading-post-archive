baf0bc132a316eae8d10b039d9d85c4dc9f97690
Mark Chimes <chimes.mark@gmail.com>
2018-02-26 13:22:52 +0200

    Leet list mapping for groups of radio buttons

--- src/View.elm
+++ src/View.elm
@@ -91,95 +91,32 @@ gridBoxWithSizeAndContent boxSize content =
     basicCell [ gridBoxElevation, size All boxSize, color1, Color.text Color.black ] content
 
 
+radioButton : Model -> String -> Bool -> Int -> String -> Html Msg
+radioButton model groupName isToggled index name =
+    Toggles.radio
+        Mdl
+        [ index ]
+        model.mdl
+        [ Toggles.value isToggled
+        , Toggles.group groupName
+        , css "padding-left" "16px"
+        , css "padding-right" "16px"
+        ]
+        [ text name ]
+
+
+radioButtons : Model -> String -> List String -> List (Html Msg)
+radioButtons model groupName names =
+    [ radioButton model groupName True 0 <| Maybe.withDefault "RADIO-BUTTON-ERROR" <| List.head names ]
+        ++ List.map2 (radioButton model groupName False) (List.range 1 <| List.length names - 1) (Maybe.withDefault [] <| List.tail names)
+
+
 viewBody : Model -> Html Msg
 viewBody model =
     div
         []
-        [ [ gridBoxWithSizeAndContent 4
-                [ Tabs.render Mdl
-                    [ 0 ]
-                    model.mdl
-                    [ Tabs.ripple
-
-                    -- , Tabs.activeTab model.tab
-                    ]
-                    [ Tabs.label
-                        [ Options.center ]
-                        [ Options.span [ css "width" "4px" ] []
-                        , text "Overview"
-                        ]
-                    , Tabs.label
-                        [ Options.center ]
-                        [ Options.span [ css "width" "4px" ] []
-                        , text "Appearance"
-                        ]
-                    ]
-                    [{--
-                 case model.tab of
-                    0 ->
-                        aboutTab
-
-                    _ ->
-                        exampleTab
-                        --}
-                    ]
-                , Options.styled p
-                    [ Typo.subhead
-                    , css "padding-top" "12px"
-                    ]
-                    [ text "Joseph McFinkelstein the Brave" ]
-                , Options.styled p
-                    [ Typo.body2 ]
-                    [ text "That guy who is looking for the magic sword" ]
-                , Button.render Mdl
-                    [ 0 ]
-                    model.mdl
-                    [ Button.raised
-                    ]
-                    [ text "Modify Description"
-                    ]
-                ]
-          , gridBoxWithSizeAndContent 4
-                [ Tabs.render Mdl
-                    [ 1 ]
-                    model.mdl
-                    [ Tabs.ripple
-
-                    -- , Tabs.activeTab model.tab
-                    ]
-                    [ Tabs.label
-                        [ Options.center ]
-                        [ Options.span [ css "width" "4px" ] []
-                        , text "Notes"
-                        ]
-                    , Tabs.label
-                        [ Options.center ]
-                        [ Options.span [ css "width" "4px" ] []
-                        , text "Log"
-                        ]
-                    , Tabs.label
-                        [ Options.center ]
-                        [ Options.span [ css "width" "4px" ] []
-                        , text "Trade"
-                        ]
-                    ]
-                    [{--
-                 case model.tab of
-                    0 ->
-                        aboutTab
-
-                    _ ->
-                        exampleTab
-                        --}
-                    ]
-                , Textfield.render Mdl
-                    [ 12 ]
-                    model.mdl
-                    [ Textfield.label "Notes"
-                    , Textfield.textarea
-                    ]
-                    []
-                ]
+        [ [ viewGridBox1 model
+          , viewGridBox2 model
           , gridBoxWithSizeAndContent 4
                 [ Options.styled p
                     [ Typo.headline ]
@@ -196,103 +133,33 @@ viewBody model =
                     ]
                 ]
           , gridBoxWithSizeAndContent 6
-                [ Options.styled p
+                ([ Options.styled p
                     [ Typo.headline ]
                     [ text "Action" ]
-                , Options.styled p
+                 , Options.styled p
                     [ Typo.subhead
                     , css "padding-top" "16px"
                     ]
                     [ text "Type of Speech" ]
-                , Toggles.radio
-                    Mdl
-                    [ 0 ]
-                    model.mdl
-                    [ Toggles.value True
-                    , Toggles.group "MyRadioGroup"
-                    , css "padding-left" "16px"
-                    , css "padding-right" "16px"
-                    ]
-                    [ text "Item Offer" ]
-                , Toggles.radio Mdl
-                    [ 1 ]
-                    model.mdl
-                    [ Toggles.value False
-                    , Toggles.group "MyRadioGroup"
-                    , css "padding-left" "16px"
-                    , css "padding-right" "16px"
-                    ]
-                    [ text "Item Request" ]
-                , Toggles.radio Mdl
-                    [ 2 ]
-                    model.mdl
-                    [ Toggles.value False
-                    , Toggles.group "MyRadioGroup"
-                    , css "padding-left" "16px"
-                    , css "padding-right" "16px"
-                    ]
-                    [ text "Listen" ]
-                , Toggles.radio Mdl
-                    [ 3 ]
-                    model.mdl
-                    [ Toggles.value False
-                    , Toggles.group "MyRadioGroup"
-                    , css "padding-left" "16px"
-                    , css "padding-right" "16px"
-                    ]
-                    [ text "Information Offer" ]
-                , Toggles.radio Mdl
-                    [ 4 ]
-                    model.mdl
-                    [ Toggles.value False
-                    , Toggles.group "MyRadioGroup"
-                    , css "padding-left" "16px"
-                    , css "padding-right" "16px"
-                    ]
-                    [ text "Inquiry" ]
-                , Toggles.radio Mdl
-                    [ 5 ]
-                    model.mdl
-                    [ Toggles.value False
-                    , Toggles.group "MyRadioGroup"
-                    , css "padding-left" "16px"
-                    , css "padding-right" "16px"
-                    ]
-                    [ text "Inform" ]
-                , Toggles.radio Mdl
-                    [ 6 ]
-                    model.mdl
-                    [ Toggles.value False
-                    , Toggles.group "MyRadioGroup"
-                    , css "padding-left" "16px"
-                    , css "padding-right" "16px"
-                    ]
-                    [ text "Chatter" ]
-                , Options.styled p
-                    [ Typo.subhead
-                    , css "padding-top" "32px"
-                    ]
-                    [ text "Tone of Voice" ]
-                , Toggles.radio
-                    Mdl
-                    [ 0 ]
-                    model.mdl
-                    [ Toggles.value True
-                    , Toggles.group "MyRadioGroup"
-                    , css "padding-left" "16px"
-                    , css "padding-right" "16px"
-                    ]
-                    [ text "Friendly" ]
-                , Toggles.radio Mdl
-                    [ 1 ]
-                    model.mdl
-                    [ Toggles.value False
-                    , Toggles.group "MyRadioGroup"
-                    , css "padding-left" "16px"
-                    , css "padding-right" "16px"
-                    ]
-                    [ text "Aggressive" ]
-                ]
+                 ]
+                    ++ radioButtons model
+                        "groupName"
+                        [ "Item Offer"
+                        , "Item Request"
+                        , "Listen"
+                        , "Information Offer"
+                        , "Inquiry"
+                        , "Inform"
+                        , "Chatter"
+                        ]
+                    ++ [ Options.styled p
+                            [ Typo.subhead
+                            , css "padding-top" "32px"
+                            ]
+                            [ text "Tone of Voice" ]
+                       ]
+                    ++ radioButtons model "group2Name" [ "Friendly", "Aggressive" ]
+                )
           , gridBoxWithSizeAndContent 6
                 [ Options.styled p
                     [ Typo.headline ]
@@ -314,4 +181,97 @@ viewBody model =
                 ]
           ]
             |> grid []
+        ]
+
+
+viewGridBox1 : Model -> Cell Msg
+viewGridBox1 model =
+    gridBoxWithSizeAndContent 4
+        [ Tabs.render Mdl
+            [ 0 ]
+            model.mdl
+            [ Tabs.ripple
+
+            -- , Tabs.activeTab model.tab
+            ]
+            [ Tabs.label
+                [ Options.center ]
+                [ Options.span [ css "width" "4px" ] []
+                , text "Overview"
+                ]
+            , Tabs.label
+                [ Options.center ]
+                [ Options.span [ css "width" "4px" ] []
+                , text "Appearance"
+                ]
+            ]
+            [{--
+                 case model.tab of
+                    0 ->
+                        aboutTab
+
+                    _ ->
+                        exampleTab
+                        --}
+            ]
+        , Options.styled p
+            [ Typo.subhead
+            , css "padding-top" "12px"
+            ]
+            [ text "Joseph McFinkelstein the Brave" ]
+        , Options.styled p
+            [ Typo.body2 ]
+            [ text "That guy who is looking for the magic sword" ]
+        , Button.render Mdl
+            [ 0 ]
+            model.mdl
+            [ Button.raised
+            ]
+            [ text "Modify Description"
+            ]
+        ]
+
+
+viewGridBox2 : Model -> Cell Msg
+viewGridBox2 model =
+    gridBoxWithSizeAndContent 4
+        [ Tabs.render Mdl
+            [ 1 ]
+            model.mdl
+            [ Tabs.ripple
+
+            -- , Tabs.activeTab model.tab
+            ]
+            [ Tabs.label
+                [ Options.center ]
+                [ Options.span [ css "width" "4px" ] []
+                , text "Notes"
+                ]
+            , Tabs.label
+                [ Options.center ]
+                [ Options.span [ css "width" "4px" ] []
+                , text "Log"
+                ]
+            , Tabs.label
+                [ Options.center ]
+                [ Options.span [ css "width" "4px" ] []
+                , text "Trade"
+                ]
+            ]
+            [{--
+                 case model.tab of
+                    0 ->
+                        aboutTab
+
+                    _ ->
+                        exampleTab
+                        --}
+            ]
+        , Textfield.render Mdl
+            [ 12 ]
+            model.mdl
+            [ Textfield.label "Notes"
+            , Textfield.textarea
+            ]
+            []
         ]
